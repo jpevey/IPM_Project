24a1f686eb3a94542f2b32c708f8aef32fffa17e
jpevey <31736945+jpevey@users.noreply.github.com>
2020-07-03 00:37:32 -0400

    setting returned keff to be (in order, if run) linear approx, keno, tsunami

--- setup_ipm.py
+++ setup_ipm.py
@@ -3,14 +3,17 @@ import os
 
 constraint_string = """function [c, ceq, DC, DCeq] = cyl_1d_cyl_constraint(variables)
 %%% This function calls a python function which:
-% original constraint: c = sum(1 - variables) -  10;
-
+% original constraint:
+c = sum(1 - variables) -  10;
 ceq = 0;
-c = -1 * sum(variables) - 9.21044037697652000E+01;
-beta_values = 10 .^ variables
-DC =  - 1 ./ beta_values;
-DC = reshape(DC,[20,1]);
-DCeq = zeros(20, 1);"""
+DC = ones(20,1) * -1;
+DCeq = zeros(20, 1);
+%%% Log constraint work:
+%c = -1 * sum(variables) - 9.21044037697652000E+01;
+%beta_values = exp(variables);
+%DC =  - 1 ./ beta_values;
+%DC = reshape(DC,[20,1]);
+%DCeq = zeros(20, 1);"""
 
 ipm_file_string = """function [x,fval,exitflag,output,lambda,grad,hessian] = cyl_1d_interior_point_algo()
 pyversion('/usr/bin/python3')
